f650edeea75672a479f687b92f034f94019e727a
Uday Kiran Jadi <udayjadi4@gmail.com>
2026-03-31 15:14:41 -0700

    conversations

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import http from "http";
 
 import { connectDB } from "./utils/db.js";
 import authRoutes from "./routes/authRoutes.js"
+import conversationRoutes from "./routes/conversationRoutes.js"
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/conversations', conversationRoutes);
 
 try {
     await connectDB();
